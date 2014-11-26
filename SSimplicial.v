e6c9e7497a565f09d2b8c9fdd5c8abc1683e3fc6
jcmckeown <jesse@sockwright>
2014-11-26 01:06:30 -0500

    And now derived cell complexes have their boundary map.
    Signed-off-by: jcmckeown <jesse@sockwright>

--- SSimplicial.v
+++ SSimplicial.v
@@ -9,31 +9,41 @@ Require Import
 Record Adjacency : Type := {
   adj_Names : nat -> Type;
   adj_Rels : 
-    forall n, (adj_Names (S n)) -> (nCkList n (S n) (adj_Names n)) -> Type
+    forall n, (adj_Names (S n)) -> (nCkList (adj_Names n) n (S n)) -> Type
   }.
 
 Record Cells (A : Adjacency) (n : nat) :={
   cell_facets : forall k, 
-    nCkList k n (adj_Names A k);
+    nCkList (adj_Names A k) k n;
   cell_glue : forall k, forall z : nCk n (S k),
-    (adj_Rels A) _ (depS_nCkS' (cell_facets (S k)) z)
-      (depS_nCkS' (nCkSubdiv (S k) (cell_facets k)) z )
+    (adj_Rels A) _ (fun_list (cell_facets (S k)) z)
+      (fun_list (nCkSubdiv (S k) (cell_facets k)) z )
       }.
 
 Definition topFacet (A : Adjacency) (n : nat) (C : Cells A n) : 
   adj_Names A n.
 Proof.
   destruct C as [ facets _ ].
-  exact ( depS_nCkS' (facets n) nCAll).
+  exact ( fun_list (facets n) nCAll).
 Defined.
 
-
-(*
 Definition boundary (A : Adjacency) (n : nat) (C : Cells A (S n)) :
-  nCkList n (S n) (Cells A n).
+  nCkList (Cells A n) n (S n).
 Proof.
   destruct C as [ facets glue ].
-  apply nCkS_depS.
-  intro.
-  assert ( help := fun k => fun z => glue k (nCkCompose x z) ).
-  refine ( Build_Cells A n _ help ). *)
+  apply list_fun.
+  intro cx.
+  exists ( fun k => (fun_list (nCkSubdiv n (facets k)) cx) ).
+  intros.
+  rewrite nCkEqn.
+  assert ( help := fun k => fun cz => glue k (nCkCompose cx cz) ).
+  refine ( transport _ _ (help _ z)).
+  unfold nCkSubdiv.
+    repeat progress ( rewrite fl_inv ).
+    apply nCkLEqn.
+    intro cz.
+    repeat progress ( rewrite fl_inv ).
+    apply ap.
+    symmetry.
+    apply nCkAssoc.
+Defined.
